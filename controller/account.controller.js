717f803c5a865b1d3c73af2fc599a8e87ff66945
someshwarholkar <someshwarsholkar2004@gmail.com>
2024-06-11 13:14:26 +0530

    initial

--- controller/account.controller.js
+++ controller/account.controller.js
@@ -1,6 +1,10 @@
 const asyncHandler = require("express-async-handler")
 const Account = require("../model/Account")
 
+exports.getAccountDetails = asyncHandler(async (re, res) => {
+    const result = await Account.find()
+    res.json({ message: "Account Deatil Fetch Success", result })
+})
 exports.credit = asyncHandler(async (req, res) => {
     await Account.create({ ...req.body, type: "credit" })
     res.json({ message: "Account Credit Success" })
--- routes/account.routes.js
+++ routes/account.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const accountController = require("../controller/account.controller")
 
 router
+    .get("/fetch", accountController.getAccountDetails)
     .post("/credit", accountController.credit)
     .post("/debit", accountController.debit)
 
